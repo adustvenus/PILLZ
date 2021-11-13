76a8707d0660096b90e178de3468214123bad2ea
NickB <j02284247@jwu.edu>
2021-11-13 14:41:50 -0500

    Add new buttons to the Gui

--- PillPlanner.py
+++ PillPlanner.py
@@ -2,24 +2,39 @@ import PySimpleGUI as sg
 
 sg.theme = 'monoblue'  # please make your windows colorful
 
-layout = [[sg.Button('Input Medication')],
-          [sg.Input(key='-IN-')],
-          [sg.Button('Show', size=(50,5)), sg.Button('Exit')]]
+layout = [
+        [sg.Button('Input Medication')],
+          [sg.Input(key='-med-')],
+          [sg.Button('Show', size=(20,1)),
+    ],
+        [sg.Button('Cost')],
+        [sg.Input(key='-cost-')],
+        [sg.Button('show', size=(20,1))
+     ],
+        [sg.Button('weekly amount')],
+        [sg.Input(key='-week-')],
+        [sg.Button('show', size=(20,1))],
+    [
+        [sg.Button("Day of the Week")],
+        [sg.Input(key="-day-")],
+        [sg.Button('show', size=(20,1))]
+    ],
+    [
+        sg.Button('exit', size=(50,10))]
+]
 
-window = sg.Window('TAKE YOUR FUCKING PILLZ!!', 
-                   layout, 
-                   size = (1000, 750),
-                   resizable = True)
+window = sg.Window('TAKE YOUR FUCKING PILLZ!!',
+                   layout,
+                   size=(1000, 750),
+                   resizable=True)
 
 while True:  # Event Loop
     event, values = window.read()
     print(event, values)
     if event == sg.WIN_CLOSED or event == 'Exit':
         break
-    if event == 'Show':
-        # change the "output" element to be the value of "input" element
-        window['-OUTPUT-'].update(values['-IN-'])
 
-window.close(
 
-#hello there
+window.close()
+
+
