bab56997cbd5d906cd6faefe8fc98309556f6eed
adustvenus <adustvenus4019@gmail.com>
2021-11-13 13:07:34 -0500

    Update PillPlanner.py

--- PillPlanner.py
+++ PillPlanner.py
@@ -1,13 +1,18 @@
 import PySimpleGUI as sg
-
 sg.theme= 'monoblue' # please make your windows colorful
 
-layout = [[sg.Button('Input Medication',element_justification='c')],
+
+a = [sg.Text('~ Pill Planner ~ ',
+             justification='center',
+             size=(100,1))]
+
+
+layout = [a,
           [sg.Input(key='-IN-')],
           [sg.Button('Show', size=(50,5)), sg.Button('Exit')]]
 
 window = sg.Window('TAKE YOUR FUCKING PILLZ!!', 
-                   layout, 
+                   layout,
                    size = (1000, 750),
                    resizable = True)
 
