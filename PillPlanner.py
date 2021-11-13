58ec4f1508961496de0716d863116f3b60e500d5
NickB <j02284247@jwu.edu>
2021-11-13 14:44:24 -0500

    Add new buttons to the Gui

--- PillPlanner.py
+++ PillPlanner.py
@@ -1,7 +1,29 @@
+import HEAD as HEAD
 import PySimpleGUI as sg
 sg.theme= 'monoblue' # please make your windows colorful
 
 
+<<<<<<< HEAD
+layout = [
+        [sg.Button('Input Medication')],
+          [sg.Input(key='-med-')],
+          [sg.Button('Show', size=(20,1)),
+    ],
+        [sg.Button('Cost')],
+        [sg.Input(key='-cost-')],
+        [sg.Button('show', size=(20,1))
+     ],
+        [sg.Button('weekly amount')],
+        [sg.Input(key='-week-')],
+        [sg.Button('show', size=(20,1))],
+    [
+        [sg.Button("Day of the Week")],
+        [sg.Input(key="-day-")],
+        [sg.Button('show', size=(20,1))]
+    ]
+]
+
+=======
 a = [sg.Text('~ Pill Planner ~ ',
              justification='center',
              size=(100,1))]
@@ -15,14 +37,19 @@ window = sg.Window('TAKE YOUR FUCKING PILLZ!!',
                    layout,
                    size = (1000, 750),
                    resizable = True)
+>>>>>>> origin/main
 
 while True:  # Event Loop
     event, values = window.read()
     print(event, values)
     if event == sg.WIN_CLOSED or event == 'Exit':
         break
-    if event == 'Show':
-        # change the "output" element to be the value of "input" element
-        window['-OUTPUT-'].update(values['-IN-'])
+
+<<<<<<< HEAD
 
 window.close()
+
+
+=======
+window.close()
+>>>>>>> origin/main
