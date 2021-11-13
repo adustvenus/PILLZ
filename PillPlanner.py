4477fd5cc4a0ae3f2a91fac4590fd943d5fff7d8
NickB <j02284247@jwu.edu>
2021-11-13 15:09:03 -0500

    Add new buttons to the Gui pt 2

--- PillPlanner.py
+++ PillPlanner.py
@@ -1,21 +1,25 @@
-import HEAD as HEAD
+
 import PySimpleGUI as sg
+
 sg.theme= 'monoblue' # please make your windows colorful
 
 
-<<<<<<< HEAD
+
 layout = [
         [sg.Button('Input Medication')],
           [sg.Input(key='-med-')],
           [sg.Button('Show', size=(20,1)),
     ],
+    [
         [sg.Button('Cost')],
         [sg.Input(key='-cost-')],
-        [sg.Button('show', size=(20,1))
-     ],
+        [sg.Button('show', size=(20,1))],
+    ],
+    [
         [sg.Button('weekly amount')],
         [sg.Input(key='-week-')],
         [sg.Button('show', size=(20,1))],
+    ],
     [
         [sg.Button("Day of the Week")],
         [sg.Input(key="-day-")],
@@ -23,21 +27,15 @@ layout = [
     ]
 ]
 
-=======
+
 a = [sg.Text('~ Pill Planner ~ ',
              justification='center',
              size=(100,1))]
-
-
-layout = [a,
-          [sg.Input(key='-IN-')],
-          [sg.Button('Show', size=(50,5)), sg.Button('Exit')]]
 
 window = sg.Window('TAKE YOUR FUCKING PILLZ!!', 
                    layout,
                    size = (1000, 750),
                    resizable = True)
->>>>>>> origin/main
 
 while True:  # Event Loop
     event, values = window.read()
@@ -45,11 +43,8 @@ while True:  # Event Loop
     if event == sg.WIN_CLOSED or event == 'Exit':
         break
 
-<<<<<<< HEAD
+
 
 window.close()
 
 
-=======
-window.close()
->>>>>>> origin/main
